c8f857a60f813d41cd92ea23d06845bd3f5155f1
Aryan Kumar <aryanajha256@gmail.com>
2023-12-01 03:09:58 +0530

    Update index.css and Chat.jsx

---
+++ src/components/RowPost.jsx
@@ -0,0 +1,13 @@
+const RowPost = () => {
+  return (
+    <div className="bg-accent px-8 py-4 rounded-md">
+      <div className="text-sm text-secondary">aryanjha256@gmail.com</div>
+      <div className="pt-2">
+        Message from the user that contains some information from the user that
+        he wants to spread regrading the topic that he has selected.
+      </div>
+    </div>
+  );
+};
+
+export default RowPost;
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,27 +1,35 @@
 import logout from "../assets/logout.svg";
 import user from "../assets/user.svg";
 import send from "../assets/send.svg";
+import RowPost from "../components/RowPost";
 
 const Chat = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <div className="flex text-center font-bold text-2xl bg-secondary py-1 px-4">
         <img src={logout} alt="logout" width={36} />
         <h1 className="mx-auto">Front Row</h1>
         <img src={user} alt="user" width={36} />
       </div>
-      <div className="flex flex-col flex-grow">
-        <div className="flex-grow"></div>
-        <div className="flex justify-center items-center h-20 bg-accent">
-          <input
-            type="text"
-            placeholder="Speak your mind..."
-            className="w-full h-12 rounded-md px-4 outline-none focus:border border-secondary"
-          />
-          <button className="bg-secondary text-white font-bold py-2 px-4 rounded-full">
-            <img src={send} alt="send" />
-          </button>
+      <div className="flex-grow overflow-y-auto">
+        <div className="max-w-4xl m-auto px-4 py-4 space-y-4">
+          <RowPost />
+          <RowPost />
+          <RowPost />
+          <RowPost />
+          <RowPost />
+          <RowPost />
         </div>
+      </div>
+      <div className="flex justify-center items-center bg-accent py-4 px-4 space-x-2">
+        <input
+          type="text"
+          placeholder="Speak your mind..."
+          className="w-full h-12 rounded-md px-4 outline-none focus:border border-secondary"
+        />
+        <button className="h-12 rounded-md px-4 bg-secondary hover:bg-primary transition-colors">
+          <img src={send} alt="send" />
+        </button>
       </div>
     </div>
   );
